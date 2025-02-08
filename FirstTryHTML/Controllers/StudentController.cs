652154e81702db73e7ee6fb9116d638114d0d0ad
gian <gian@LAPTOP-9OTEDHFM>
2025-02-08 13:51:17 +0800

    Add project files.

---
+++ FirstTryHTML/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTryHTML.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+    }
+}
